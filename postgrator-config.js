b834af9b9e500c0f16f79d9b3b5c059d7f170948
iqsymphic <iqsymphic@gmail.com>
2020-03-12 15:34:21 -0400

    commit

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,8 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-  migrationsDirectory: "migrations",
-  driver: "pg",
-  connectionString:
-    process.env.DATABASE_URL === "test"
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL
-};
+    "migrationsDirectory": "migrations",
+    "driver": "pg",
+    "connectionString": process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL,
+}
--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/blogful",
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
-    "postgresql://dunder_mifflin@localhost/blogful-test"
-};
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: 'postgres://vbmhhsduahsnng:f0f68dd5df0be93ac7ccf22a02e3e580208c08d324cb9df7f463261fd723b13c@ec2-174-129-238-192.compute-1.amazonaws.com:5432/d45i7e34d9ene9',
+    // DATABASE_URL: process.env.DATABASE_URL
+}
